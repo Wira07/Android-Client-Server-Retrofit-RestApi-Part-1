31e7bcd098567b4492a188791f8572e1f85c39ce
Wira07 <wiralodrasaputra07@gmail.com>
2024-05-22 20:23:01 +0700

    Initial commit

---
+++ app/src/main/java/com/wira_fkom/android_client/ApiClient.java
@@ -0,0 +1,19 @@
+package com.wira_fkom.android_client;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+public class ApiClient {
+    //link base url disesuaikan dengan Alamat ip masing-masing,
+    private static final String BASE_URL = "http://192.168.253.187/Android_API/";
+    private static Retrofit retrofit = null;
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
+
---
+++ app/src/main/java/com/wira_fkom/android_client/User.java
@@ -0,0 +1,13 @@
+package com.wira_fkom.android_client;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email; }
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+}
